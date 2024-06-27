0dcbaef0b078f9620ec0d3c8905931b4efe57760
Vlad <vladik11012006@gmail.com>
2024-06-27 23:19:23 +0200

    cursor management done + discord button

---
+++ Assets/Scripts/CursorScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class CursorScript : MonoBehaviour
+{
+    public Player owner;
+}
---
+++ Assets/Scripts/DiscordButtonScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscordButtonScript : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Application.OpenURL("https://discord.com/");
+    }
+}
